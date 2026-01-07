c5e673f66d80ddf1bccfc291a73f52653c480b37
Akhil100605 <saiakhilchandradevalla52@gmail.com>
2026-01-07 15:50:29 +0530

    Cmaera with runtime permission

--- main.py
+++ main.py
@@ -1,9 +1,47 @@
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.camera import Camera
 from kivy.uix.label import Label
 
-class TestApp(App):
+from android.permissions import request_permissions, Permission
+
+
+class CamApp(App):
     def build(self):
-        return Label(text="Hello from Kivy 😎")
+        request_permissions([Permission.CAMERA])
+
+        layout = BoxLayout(orientation="vertical")
+
+        self.cam = Camera(play=True, resolution=(640, 480))
+        layout.add_widget(self.cam)
+
+        layout.add_widget(Label(text="Camera ON 📸"))
+
+        return layout
+
 
 if __name__ == "__main__":
-    TestApp().run()
+    CamApp().run()from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.camera import Camera
+from kivy.uix.label import Label
+
+from android.permissions import request_permissions, Permission
+
+
+class CamApp(App):
+    def build(self):
+        request_permissions([Permission.CAMERA])
+
+        layout = BoxLayout(orientation="vertical")
+
+        self.cam = Camera(play=True, resolution=(640, 480))
+        layout.add_widget(self.cam)
+
+        layout.add_widget(Label(text="Camera ON 📸"))
+
+        return layout
+
+
+if __name__ == "__main__":
+    CamApp().run()
