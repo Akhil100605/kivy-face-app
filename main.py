b937f58430685bf6f64efe87f3194d18b05711f1
Akhil100605 <saiakhilchandradevalla52@gmail.com>
2026-01-07 16:27:39 +0530

    Correct Orientation

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.camera import Camera
 from kivy.uix.label import Label
+from kivy.clock import Clock
 
 from android.permissions import request_permissions, Permission
 
@@ -13,11 +14,20 @@ class CamApp(App):
         layout = BoxLayout(orientation="vertical")
 
         self.cam = Camera(play=True, resolution=(640, 480))
-        layout.add_widget(self.cam)
+        self.cam.allow_stretch = True
+        self.cam.keep_ratio = True
 
+        layout.add_widget(self.cam)
         layout.add_widget(Label(text="Camera ON 📸"))
 
+        Clock.schedule_once(self.fix_rotation, 1)
         return layout
+
+    def fix_rotation(self, dt):
+        # rotate camera texture to portrait
+        if self.cam.texture:
+            self.cam.texture.flip_vertical()
+            self.cam.rotation = 90
 
 
 if __name__ == "__main__":
