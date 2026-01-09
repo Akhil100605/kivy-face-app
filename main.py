571dbed9dd05c0a5b597aaeb4181b0699f905388
Akhil100605 <saiakhilchandradevalla52@gmail.com>
2026-01-09 15:48:10 +0530

    I am ready

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.camera import Camera
 from kivy.uix.button import Button
 from kivy.core.window import Window
-from kivy.graphics import Scale, Translate
-
+from kivy.graphics import Rotate, Scale, PushMatrix, PopMatrix
 from android.permissions import request_permissions, Permission
 
 
@@ -12,10 +11,9 @@ class CameraApp(App):
     def build(self):
         request_permissions([Permission.CAMERA])
 
-        Window.rotation = 0
+        Window.rotation = 0  # keep app portrait
 
         self.cam_index = 0
-
         self.layout = BoxLayout(orientation='vertical')
 
         self.add_camera()
@@ -30,19 +28,19 @@ class CameraApp(App):
         self.camera = Camera(index=self.cam_index, play=True)
 
         with self.camera.canvas.before:
+            PushMatrix()
+            self.rot = Rotate(angle=90, origin=self.camera.center)
             self.scale = Scale(x=-1 if self.cam_index == 1 else 1, y=1, z=1)
-            self.translate = Translate()
 
-        self.camera.bind(size=self.update_transform, pos=self.update_transform)
+        with self.camera.canvas.after:
+            PopMatrix()
+
+        self.camera.bind(pos=self.update_transform, size=self.update_transform)
 
         self.layout.add_widget(self.camera, index=1)
 
     def update_transform(self, *args):
-        if self.cam_index == 1:  # mirror only front cam
-            self.translate.x = -self.camera.width
-        else:
-            self.translate.x = 0
-        self.translate.y = 0
+        self.rot.origin = self.camera.center
 
     def switch_camera(self, *args):
         self.layout.remove_widget(self.camera)
@@ -54,4 +52,5 @@ class CameraApp(App):
 
 if __name__ == "__main__":
     CameraApp().run()
+
 
