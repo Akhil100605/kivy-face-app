b9049aa7f75bcc8a885263be87c613815fe3f7a3
Akhil100605 <saiakhilchandradevalla52@gmail.com>
2026-01-09 14:51:53 +0530

    I am ready

--- main.py
+++ main.py
@@ -1,30 +1,37 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.camera import Camera
 from kivy.uix.button import Button
+from android.permissions import request_permissions, Permission
 
-from camera4kivy import Preview
-
-
-class CamApp(App):
-
+request_permissions([
+    Permission.CAMERA,
+    Permission.READ_EXTERNAL_STORAGE,
+    Permission.WRITE_EXTERNAL_STORAGE
+])
+class CameraApp(App):
     def build(self):
-        layout = BoxLayout(orientation="vertical")
+        request_permissions([Permission.CAMERA])
 
-        self.preview = Preview(play=True)
+        self.cam_index = 0
+
+        layout = BoxLayout(orientation='vertical')
+
+        self.camera = Camera(index=self.cam_index, play=True)
+
+        layout.add_widget(self.camera)
 
         btn = Button(text="Switch Camera", size_hint_y=None, height=60)
         btn.bind(on_press=self.switch_camera)
-
-        layout.add_widget(self.preview)
         layout.add_widget(btn)
+
         return layout
 
     def switch_camera(self, *args):
-        if self.preview.camera_id == "0":
-            self.preview.camera_id = "1"
-        else:
-            self.preview.camera_id = "0"
+        self.cam_index = 1 if self.cam_index == 0 else 0
+        self.camera.index = self.cam_index
 
 
 if __name__ == "__main__":
-    CamApp().run()
+    CameraApp().run()
+
