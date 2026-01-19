fe3c523d9fde462215897d90d25953f18b6331ad
Akhil100605 <saiakhilchandradevalla52@gmail.com>
2026-01-19 11:33:02 +0530

    I am ready

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from kivy app import App
+from kivy.app import App
 from kivy.uix.label import Label
+
 class TestApp(App):
     def build(self):
-        return Label(text="APP OPENED SUCCESSFULLY",font_size=10)
-if _name_=='_main_':
+        return Label(text="APP OPENED SUCCESSFULLY", font_size=30)
+
+if __name__ == "__main__":
     TestApp().run()
