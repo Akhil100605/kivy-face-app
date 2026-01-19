fc0065a85a28abce6fb40b939557f4de30662521
Akhil100605 <saiakhilchandradevalla52@gmail.com>
2026-01-19 13:09:22 +0530

    I am ready

--- main.py
+++ main.py
@@ -6,22 +6,32 @@ from kivy.graphics.texture import Texture
 import cv2
 import numpy as np
 
+from android.permissions import request_permissions, Permission
+
 
 class CamApp(App):
     def build(self):
         self.img = Image()
-        self.cap = cv2.VideoCapture(0)
+        self.cap = None
 
-        Clock.schedule_interval(self.update, 1 / 30)
+        request_permissions([Permission.CAMERA], self.on_permission)
         return self.img
 
+    def on_permission(self, permissions, results):
+        if all(results):
+            self.cap = cv2.VideoCapture(0)
+            Clock.schedule_interval(self.update, 1 / 30)
+
     def update(self, dt):
+        if self.cap is None:
+            return
+
         ret, frame = self.cap.read()
         if not ret:
             return
 
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        frame = cv2.flip(frame, 1)  # mirror fix
+        frame = cv2.flip(frame, 1)
 
         texture = Texture.create(
             size=(frame.shape[1], frame.shape[0]),
@@ -35,8 +45,11 @@ class CamApp(App):
         self.img.texture = texture
 
     def on_stop(self):
-        self.cap.release()
+        if self.cap:
+            self.cap.release()
 
 
 if __name__ == "__main__":
     CamApp().run()
+
+
